4b08df217b88ba4f8aba5288adbdd9dab08a939e
dasWarder <Al.94_94@mail.ru>
2021-06-23 09:02:30 +0300

    Add more controller methods

--- src/main/java/by/itechart/fullstack_demo/controller/UserController.java
+++ src/main/java/by/itechart/fullstack_demo/controller/UserController.java
@@ -3,14 +3,14 @@ package by.itechart.fullstack_demo.controller;
 import by.itechart.fullstack_demo.model.User;
 import by.itechart.fullstack_demo.repository.UserJpaRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Slf4j
-@RestController
+@RestController(value = "/users")
 @CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
@@ -21,13 +21,61 @@ public class UserController {
     }
 
 
-    @GetMapping("/users")
-    public List<User> getAllUsers() {
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
 
         log.info("Receiving a list of all users");
         List<User> users = userJpaRepository.findAll();
 
-        return users;
+        return new ResponseEntity<>(
+                                    users,
+                                    HttpStatus.OK);
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
+
+        log.info("Receive a single user by ID = {}", id);
+        User userFromDb = userJpaRepository.getOne(id);
+
+        return new ResponseEntity<>(
+                                    userFromDb,
+                                    HttpStatus.OK);
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+
+        log.info("Store a single user into the database");
+        User storedUser = userJpaRepository.save(user);
+
+        return new ResponseEntity<>(
+                                    storedUser,
+                                    HttpStatus.CREATED);
+    }
+
+    @PutMapping("/user/{id}")
+    public ResponseEntity<User> updateSingleUser(@PathVariable("id") Long id,
+                                                 @RequestBody User userForUpdating) {
+
+        log.info("Update a single user with ID = {}", id);
+        User userFromDb = userJpaRepository.getOne(id);
+        userForUpdating.setId(userFromDb.getId());
+
+        User storedUpdatedUser = userJpaRepository.save(userForUpdating);
+
+        return new ResponseEntity<>(
+                                    storedUpdatedUser,
+                                    HttpStatus.OK);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity deleteOne(@PathVariable("id") Long id) {
+
+        log.info("Delete a single user by ID = {}", id);
+        userJpaRepository.deleteById(id);
+
+        return new ResponseEntity(HttpStatus.OK);
     }
 
 }
